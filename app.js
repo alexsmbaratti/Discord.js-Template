adfeb71d00ee8d4f6e518dbac603ea4bc2441bf1
Alex Baratti <32379831+alexsmbaratti@users.noreply.github.com>
2018-02-06 11:21:49 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+var config = require('./config.json');
+const client = new Discord.Client();
+
+const build = "0.0.1"; // Change with each update
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+  console.log("Build: " + build);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase().substring(0, 2) == `YOUR_PREFIX`) { // Replace YOUR_PREFIX with something like a!
+    switch (msg.content.toLowerCase().substring(2)) {
+      case `COMMAND`: // Add your commands here
+        break;
+    }}});
+
+client.login(config.TOKEN);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setGame(game) {
+  console.log("Set current game to " + game);
+  client.user.setPresence({
+    status: 'online',
+    game: {
+      name: game
+    }
+  });
+}
